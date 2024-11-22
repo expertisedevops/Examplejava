f3eb546518e94f39229138fd658a10fff95ef029
expertisedevops <expertisedevops@gmail.com>
2024-11-22 19:17:10 +0530

    Update Example.java

--- Example.java
+++ Example.java
@@ -1,6 +1,6 @@
 public class Example {
     public static void main(String[] args) {
         // Print Hello, World! to the console
-        System.out.println("Hello, DevOps Engineers \n Welcome to DevOps classes!!");
+        System.out.println("Hello, DevOps Engineers \n Welcome to DevOps classes!!\n Docker \n Linux \n Kubernetes \n Terraform \n Ansible");
     }
 }
