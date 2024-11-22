ea0765d62defc8b55e084d37cb7b4e60f74ee11a
prashanth <nocturnaldevops@gmail.com>
2024-11-22 19:15:38 +0530

    third commit

--- Example.java
+++ Example.java
@@ -1,6 +1,6 @@
 public class Example {
     public static void main(String[] args) {
         // Print Hello, World! to the console
-        System.out.println("Hello, DevOps Engineers!!");
+        System.out.println("Hello, DevOps Engineers \n Welcome to DevOps classes!!");
     }
 }
